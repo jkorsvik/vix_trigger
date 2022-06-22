e16296b93e486c8970a70dcd45d13cb5f390c1b7
Jon-Mikkel Korsvik <jonkors@nmbu.no>
2022-06-22 23:23:07 +0200

    fixing imports in model

--- src/model.py
+++ src/model.py
@@ -9,11 +9,11 @@ import pandas as pd
 
 import pandas as pd
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
-from keras.layers import *
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import *
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
